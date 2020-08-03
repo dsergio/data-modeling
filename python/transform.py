90b005c997c5814a83c83a47d2bbd413948c4f4f
David Sergio <davidsergio52@gmail.com>
2020-08-02 21:12:11 -0700

    comments and rapidminer project files

--- python/transform.py
+++ python/transform.py
@@ -37,7 +37,7 @@ transformPathStage6 = "..\\transform\\stage6"
 
 #################
 # Stage 1
-# Description: TBD
+# Description: Deal with missing data and group weather station values by mean
 extractFilesStage1 = [f for f in listdir(extractPathStage1) if isfile(join(extractPathStage1, f))]
 for file in extractFilesStage1:
 	print("file: " + file)
@@ -46,35 +46,35 @@ for file in extractFilesStage1:
 
 #################
 # Stage 2
-# Description: TBD
+# Description: Put all dates from stage1 into one CSV file
 transformFileStage2(transformPathStage1, transformPathStage2)
 
 
 #################
 # Stage 3
-# Description: TBD
+# Description: Separate - this time by Zone and Elevation
 transformFileStage3(transformPathStage2, transformPathStage3)
 
 
 #################
 # Stage 4
-# Description: TBD
+# Description: Calculate 'storm' and 'days_since_storm' metrics and append to each CSV
 transformFileStage4(transformPathStage3, transformPathStage4)
 
 
 #################
 # Stage 5a
-# Description: TBD
+# Description: Resolve differences in Zone names for observation data
 transformFileStage5a(observationExtractStage1File, transformPathStage4, transformPathStage5a)
 
 
 #################
 # Stage 5b
-# Description: TBD
+# Description: Using weather data, query observation data (similar to join except get count -- I'm not sure how to do this in Python)
 transformFileStage5b(transformPathStage5a, transformPathStage4, transformPathStage5b)
 
 
 #################
 # Stage 6
-# Description: TBD
+# Description: Combine all data again into a single CSV file
 transformFileStage6(transformPathStage5b, transformPathStage6)
